5515f712c4df1166d33a6cb29623e4e0022e941e
Dương Diệu Pháp <d2phap@gmail.com>
2014-03-05 00:35:15 +0700

    New plugin: Image Comparator

---
+++ Source/Plugins/Image Comparator/Image Comparator/DirectoryTool.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image_Comparator
+{
+    public static class DirectoryTool
+    {
+        #region DirectoryList
+        /// <summary>
+        /// Returns a list of directories under RootDirectory 
+        /// </summary>
+        /// <param name="RootDirectory">starting directory</param>
+        /// <param name="SearchAllDirectories">when true, all sub directories will be searched as well</param>
+        /// <param name="Filter">filter to be done on directory. use null for no filtering</param>
+        /// <returns></returns>
+        public static List<string> DirectoryList(string RootDirectory,
+            bool SearchAllDirectories, Predicate<string> Filter)
+        {
+            List<string> retList = new List<string>();
+
+            try
+            {
+                // create a directory info object 
+                DirectoryInfo di = new DirectoryInfo(RootDirectory);
+
+                // loop through directories populating the list 
+                Parallel.ForEach(di.GetDirectories(), folder =>
+                {
+                    try
+                    {
+                        // add the folder if it passes the filter 
+                        if ((Filter == null) || (Filter(folder.FullName)))
+                        {
+                            // add the folder 
+                            retList.Add(folder.FullName);
+
+                            // get it's sub folders 
+                            if (SearchAllDirectories)
+                                retList.AddRange(DirectoryList(folder.FullName, true,
+                                    Filter));
+                        }
+                    }
+
+                    catch (UnauthorizedAccessException)
+                    {
+                        // don't really need to do anything 
+                        // user just doesn't have access 
+                    }
+
+                    catch (Exception excep)
+                    {
+                        // TODO: log the exception 
+                    }
+                });
+            }
+
+            catch (Exception excep)
+            {
+                // TODO: save exception 
+            }
+
+            // return the list 
+            return retList;
+        }
+
+        // DirectoryList 
+        #endregion
+
+        #region FileList
+
+        /// <summary>
+        /// Returns a list of files under RootDirectory
+        /// </summary>
+        /// <param name="RootDirectory">>starting directory</param>
+        /// <param name="SearchAllDirectories">>when true, all sub directories will be searched as well</param>
+        /// <param name="Filter">filter to be done on files/directory. use null for no filtering</param>
+        /// <returns></returns>
+        public static List<string> FileList(string RootDirectory,
+            bool SearchAllDirectories, Predicate<string> Filter)
+        {
+            List<string> retList = new List<string>();
+
+            try
+            {
+                // get the list of directories 
+                List<string> DirList = new List<string> { RootDirectory };
+
+                // get sub directories if allowed 
+                if (SearchAllDirectories)
+                    DirList.AddRange(DirectoryList(RootDirectory, true, null));
+
+                // loop through directories populating the list 
+                Parallel.ForEach(DirList, folder =>
+                {
+                    // get a directory object 
+                    DirectoryInfo di = new DirectoryInfo(folder);
+
+                    try
+                    {
+                        // loop through the files in this directory 
+                        foreach (FileInfo file in di.GetFiles())
+                        {
+                            try
+                            {
+                                // add the file if it passes the filter 
+                                if ((Filter == null) || (Filter(file.FullName)))
+                                    retList.Add(file.FullName);
+                            }
+
+                            catch (Exception excep)
+                            {
+                                // TODO: log the exception 
+                            }
+                        }
+                    }
+
+                    catch (UnauthorizedAccessException)
+                    {
+                        // don't really need to do anything 
+                        // user just doesn't have access 
+                    }
+
+                    catch (Exception excep)
+                    {
+                        // TODO: log the exception 
+                    }
+                });
+            }
+
+            catch (Exception excep)
+            {
+                // TODO: save exception 
+            }
+
+            // return the list 
+            return retList;
+        }
+
+        // FileList 
+        #endregion 
+    }
+}
---
+++ Source/Plugins/Image Comparator/Image Comparator/ImageComparator.Designer.cs
@@ -0,0 +1,333 @@
+﻿using System.Drawing;
+namespace Image_Comparator
+{
+    partial class ImageComparator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panBot = new System.Windows.Forms.Panel();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.btnChangeImage = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lnkAbout = new System.Windows.Forms.LinkLabel();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.radImage = new System.Windows.Forms.RadioButton();
+            this.btnChooseFoler = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.btnChooseImage = new System.Windows.Forms.Button();
+            this.txtImagePath = new System.Windows.Forms.TextBox();
+            this.radFolder = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lnkCopy = new System.Windows.Forms.LinkLabel();
+            this.panBot.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Image Comparator";
+            // 
+            // panBot
+            // 
+            this.panBot.BackColor = System.Drawing.Color.Transparent;
+            this.panBot.Controls.Add(this.btnCompare);
+            this.panBot.Controls.Add(this.btnChangeImage);
+            this.panBot.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panBot.Location = new System.Drawing.Point(0, 359);
+            this.panBot.Name = "panBot";
+            this.panBot.Size = new System.Drawing.Size(645, 56);
+            this.panBot.TabIndex = 1;
+            this.panBot.Paint += new System.Windows.Forms.PaintEventHandler(this.panBot_Paint);
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCompare.Location = new System.Drawing.Point(530, 10);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(89, 31);
+            this.btnCompare.TabIndex = 1;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // btnChangeImage
+            // 
+            this.btnChangeImage.Location = new System.Drawing.Point(29, 10);
+            this.btnChangeImage.Name = "btnChangeImage";
+            this.btnChangeImage.Size = new System.Drawing.Size(152, 31);
+            this.btnChangeImage.TabIndex = 0;
+            this.btnChangeImage.Text = "Change current image";
+            this.btnChangeImage.UseVisualStyleBackColor = true;
+            this.btnChangeImage.Click += new System.EventHandler(this.btnChangeImage_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Compare images to see if they were identical";
+            // 
+            // lnkAbout
+            // 
+            this.lnkAbout.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(131)))), ((int)(((byte)(244)))));
+            this.lnkAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lnkAbout.AutoSize = true;
+            this.lnkAbout.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(131)))), ((int)(((byte)(244)))));
+            this.lnkAbout.Location = new System.Drawing.Point(579, 47);
+            this.lnkAbout.Name = "lnkAbout";
+            this.lnkAbout.Size = new System.Drawing.Size(40, 15);
+            this.lnkAbout.TabIndex = 4;
+            this.lnkAbout.TabStop = true;
+            this.lnkAbout.Text = "About";
+            this.lnkAbout.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(131)))), ((int)(((byte)(244)))));
+            this.lnkAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkAbout_LinkClicked);
+            // 
+            // pic
+            // 
+            this.pic.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic.Location = new System.Drawing.Point(29, 98);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(335, 242);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic.TabIndex = 5;
+            this.pic.TabStop = false;
+            this.pic.Tag = "aaa";
+            // 
+            // radImage
+            // 
+            this.radImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radImage.AutoSize = true;
+            this.radImage.Checked = true;
+            this.radImage.Location = new System.Drawing.Point(390, 105);
+            this.radImage.Name = "radImage";
+            this.radImage.Size = new System.Drawing.Size(88, 19);
+            this.radImage.TabIndex = 6;
+            this.radImage.TabStop = true;
+            this.radImage.Text = "Image path:";
+            this.radImage.UseVisualStyleBackColor = true;
+            // 
+            // btnChooseFoler
+            // 
+            this.btnChooseFoler.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseFoler.Location = new System.Drawing.Point(585, 184);
+            this.btnChooseFoler.Name = "btnChooseFoler";
+            this.btnChooseFoler.Size = new System.Drawing.Size(34, 23);
+            this.btnChooseFoler.TabIndex = 11;
+            this.btnChooseFoler.Text = "...";
+            this.btnChooseFoler.UseVisualStyleBackColor = true;
+            this.btnChooseFoler.Click += new System.EventHandler(this.btnChooseFoler_Click);
+            this.btnChooseFoler.Enter += new System.EventHandler(this.txtFolderPath_Enter);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolderPath.Location = new System.Drawing.Point(408, 184);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(171, 23);
+            this.txtFolderPath.TabIndex = 10;
+            this.txtFolderPath.Enter += new System.EventHandler(this.txtFolderPath_Enter);
+            // 
+            // btnChooseImage
+            // 
+            this.btnChooseImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseImage.Location = new System.Drawing.Point(585, 130);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(34, 23);
+            this.btnChooseImage.TabIndex = 9;
+            this.btnChooseImage.Text = "...";
+            this.btnChooseImage.UseVisualStyleBackColor = true;
+            this.btnChooseImage.Click += new System.EventHandler(this.btnChooseImage_Click);
+            this.btnChooseImage.Enter += new System.EventHandler(this.txtImagePath_Enter);
+            // 
+            // txtImagePath
+            // 
+            this.txtImagePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtImagePath.Location = new System.Drawing.Point(408, 130);
+            this.txtImagePath.Name = "txtImagePath";
+            this.txtImagePath.Size = new System.Drawing.Size(171, 23);
+            this.txtImagePath.TabIndex = 8;
+            this.txtImagePath.Enter += new System.EventHandler(this.txtImagePath_Enter);
+            // 
+            // radFolder
+            // 
+            this.radFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radFolder.AutoSize = true;
+            this.radFolder.Location = new System.Drawing.Point(390, 159);
+            this.radFolder.Name = "radFolder";
+            this.radFolder.Size = new System.Drawing.Size(88, 19);
+            this.radFolder.TabIndex = 7;
+            this.radFolder.TabStop = true;
+            this.radFolder.Text = "Folder path:";
+            this.radFolder.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Current image:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(387, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Options:";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.lnkCopy);
+            this.panel1.Controls.Add(this.lblResult);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(382, 213);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 127);
+            this.panel1.TabIndex = 13;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(23, 34);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(214, 93);
+            this.lblResult.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(5, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Result:";
+            // 
+            // lnkCopy
+            // 
+            this.lnkCopy.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(131)))), ((int)(((byte)(244)))));
+            this.lnkCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lnkCopy.AutoSize = true;
+            this.lnkCopy.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(131)))), ((int)(((byte)(244)))));
+            this.lnkCopy.Location = new System.Drawing.Point(170, 12);
+            this.lnkCopy.Name = "lnkCopy";
+            this.lnkCopy.Size = new System.Drawing.Size(67, 15);
+            this.lnkCopy.TabIndex = 14;
+            this.lnkCopy.TabStop = true;
+            this.lnkCopy.Text = "Copy result";
+            this.lnkCopy.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(131)))), ((int)(((byte)(244)))));
+            this.lnkCopy.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkCopy_LinkClicked);
+            // 
+            // ImageComparator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnChooseFoler);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.btnChooseImage);
+            this.Controls.Add(this.panBot);
+            this.Controls.Add(this.txtImagePath);
+            this.Controls.Add(this.lnkAbout);
+            this.Controls.Add(this.radFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radImage);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimumSize = new System.Drawing.Size(645, 415);
+            this.Name = "ImageComparator";
+            this.Size = new System.Drawing.Size(645, 415);
+            this.Load += new System.EventHandler(this.ImageComparator_Load);
+            this.SizeChanged += new System.EventHandler(this.ImageComparator_SizeChanged);
+            this.panBot.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panBot;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Button btnChangeImage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel lnkAbout;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.RadioButton radImage;
+        private System.Windows.Forms.RadioButton radFolder;
+        private System.Windows.Forms.Button btnChooseFoler;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Button btnChooseImage;
+        private System.Windows.Forms.TextBox txtImagePath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.ToolTip tip1;
+        private System.Windows.Forms.LinkLabel lnkCopy;
+    }
+}
