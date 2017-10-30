ea9d7eb23167c3c1e161be3403f5bcb867474b40
Phap Dieu Duong <d2phap@gmail.com>
2017-10-30 14:45:26 +0800

    Update version + langauge packs

--- Source/Components/ImageGlass.Services/Configuration/GlobalSetting.cs
+++ Source/Components/ImageGlass.Services/Configuration/GlobalSetting.cs
@@ -50,7 +50,7 @@ namespace ImageGlass.Services.Configuration
         private static bool _isStartUpDirWritable = true;
         private static bool _isTempMemoryData = false;
         private static ConfigurationFile _configFile = new ConfigurationFile();
-        private static string _builtInImageFormats = "*.bmp;*.cur;*.cut;*.dib;*.emf;*.exif;*.gif;*.ico;*.jfif;*.jpe;*.jpeg;*.jpg;*.pbm;*.pcx;*.pgm;*.png;*.ppm;*.psb;*.svg;*.tif;*.tiff;*.webp;*.wmf;*.wpg;*.xbm;*.xpm;|*.exr;*.hdr;*.psd;*.tga;";
+        private static string _builtInImageFormats = "*.bmp;*.cur;*.cut;*.dds;*.dib;*.emf;*.exif;*.gif;*.ico;*.jfif;*.jpe;*.jpeg;*.jpg;*.pbm;*.pcx;*.pgm;*.png;*.ppm;*.psb;*.svg;*.tif;*.tiff;*.webp;*.wmf;*.wpg;*.xbm;*.xpm;|*.exr;*.hdr;*.psd;*.tga;";
         private static int _settingsTabLastView = 0;
         
 
