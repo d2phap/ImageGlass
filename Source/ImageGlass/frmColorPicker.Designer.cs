b8f441d9af1f58e63628596004fbb4522267b60b
Phap Dieu Duong <d2phap@gmail.com>
2018-04-11 15:50:25 +0800

    Updated UI
    - Made borderless form
    - Made form movable
    - Move parent form will also move color picker form
    - Change color code to textbox

--- Source/ImageGlass/frmColorPicker.Designer.cs
+++ Source/ImageGlass/frmColorPicker.Designer.cs
@@ -32,58 +32,95 @@
             this.panelColor = new System.Windows.Forms.Panel();
             this.lblRgb = new System.Windows.Forms.Label();
             this.lblHex = new System.Windows.Forms.Label();
+            this.txtRGB = new System.Windows.Forms.TextBox();
+            this.txtHEX = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblPixel
             // 
             this.lblPixel.AutoSize = true;
             this.lblPixel.ForeColor = System.Drawing.Color.White;
-            this.lblPixel.Location = new System.Drawing.Point(12, 41);
+            this.lblPixel.Location = new System.Drawing.Point(15, 70);
+            this.lblPixel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPixel.Name = "lblPixel";
-            this.lblPixel.Size = new System.Drawing.Size(31, 15);
+            this.lblPixel.Size = new System.Drawing.Size(47, 25);
             this.lblPixel.TabIndex = 0;
-            this.lblPixel.Text = "pixel";
+            this.lblPixel.Text = "Pixel";
+            this.lblPixel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmColorPicker_MouseDown);
             // 
             // panelColor
             // 
+            this.panelColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panelColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelColor.Location = new System.Drawing.Point(12, 13);
+            this.panelColor.Location = new System.Drawing.Point(20, 20);
+            this.panelColor.Margin = new System.Windows.Forms.Padding(4);
             this.panelColor.Name = "panelColor";
-            this.panelColor.Size = new System.Drawing.Size(110, 25);
+            this.panelColor.Size = new System.Drawing.Size(224, 36);
             this.panelColor.TabIndex = 1;
+            this.panelColor.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmColorPicker_MouseDown);
             // 
             // lblRgb
             // 
             this.lblRgb.AutoSize = true;
             this.lblRgb.ForeColor = System.Drawing.Color.White;
-            this.lblRgb.Location = new System.Drawing.Point(12, 56);
+            this.lblRgb.Location = new System.Drawing.Point(15, 108);
+            this.lblRgb.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRgb.Name = "lblRgb";
-            this.lblRgb.Size = new System.Drawing.Size(25, 15);
+            this.lblRgb.Size = new System.Drawing.Size(49, 25);
             this.lblRgb.TabIndex = 2;
-            this.lblRgb.Text = "rgb";
+            this.lblRgb.Text = "RGB:";
+            this.lblRgb.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmColorPicker_MouseDown);
             // 
             // lblHex
             // 
             this.lblHex.AutoSize = true;
             this.lblHex.ForeColor = System.Drawing.Color.White;
-            this.lblHex.Location = new System.Drawing.Point(12, 71);
+            this.lblHex.Location = new System.Drawing.Point(15, 145);
+            this.lblHex.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHex.Name = "lblHex";
-            this.lblHex.Size = new System.Drawing.Size(25, 15);
+            this.lblHex.Size = new System.Drawing.Size(49, 25);
             this.lblHex.TabIndex = 3;
-            this.lblHex.Text = "hex";
+            this.lblHex.Text = "HEX:";
+            this.lblHex.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmColorPicker_MouseDown);
+            // 
+            // txtRGB
+            // 
+            this.txtRGB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(74)))), ((int)(((byte)(72)))));
+            this.txtRGB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRGB.Location = new System.Drawing.Point(71, 105);
+            this.txtRGB.Name = "txtRGB";
+            this.txtRGB.ReadOnly = true;
+            this.txtRGB.Size = new System.Drawing.Size(173, 31);
+            this.txtRGB.TabIndex = 4;
+            this.txtRGB.Click += new System.EventHandler(this.ColorTextbox_Click);
+            // 
+            // txtHEX
+            // 
+            this.txtHEX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(74)))), ((int)(((byte)(72)))));
+            this.txtHEX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHEX.Location = new System.Drawing.Point(71, 142);
+            this.txtHEX.Name = "txtHEX";
+            this.txtHEX.ReadOnly = true;
+            this.txtHEX.Size = new System.Drawing.Size(173, 31);
+            this.txtHEX.TabIndex = 5;
+            this.txtHEX.Click += new System.EventHandler(this.ColorTextbox_Click);
             // 
             // frmColorPicker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(74)))), ((int)(((byte)(72)))));
-            this.ClientSize = new System.Drawing.Size(134, 111);
+            this.ClientSize = new System.Drawing.Size(264, 200);
+            this.Controls.Add(this.txtHEX);
+            this.Controls.Add(this.txtRGB);
             this.Controls.Add(this.lblHex);
             this.Controls.Add(this.lblRgb);
             this.Controls.Add(this.panelColor);
             this.Controls.Add(this.lblPixel);
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmColorPicker";
@@ -94,8 +131,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Color Picker";
             this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmColorPicker_MouseDown);
             this.MouseEnter += new System.EventHandler(this.frmColorPicker_MouseEnter);
             this.MouseLeave += new System.EventHandler(this.frmColorPicker_MouseLeave);
+            this.Move += new System.EventHandler(this.frmColorPicker_Move);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,5 +146,7 @@
         private System.Windows.Forms.Panel panelColor;
         private System.Windows.Forms.Label lblRgb;
         private System.Windows.Forms.Label lblHex;
+        private System.Windows.Forms.TextBox txtRGB;
+        private System.Windows.Forms.TextBox txtHEX;
     }
 }
--- Source/ImageGlass/frmColorPicker.cs
+++ Source/ImageGlass/frmColorPicker.cs
@@ -7,25 +7,34 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ImageGlass.Theme;
+using System.Runtime.InteropServices;
 
 namespace ImageGlass
 {
     public partial class frmColorPicker : Form
     {
+
         // opacity when the mouse is out of this form
         private const double FormOpacity = 0.9;
-
         // default location offset on the parent form
-        private static readonly Point DefaultLocationOffset = new Point(30, 90);
+        private static Point DefaultLocationOffset = new Point(20, 80);
 
         private Form _currentOwner = null;
         private ImageBox _imgBox = null;
         private BitmapBooster _bmpBooster = null;
         private Point _locationOffset = DefaultLocationOffset;
+        private Point _cursorPos = new Point();
+
 
         public frmColorPicker()
         {
             InitializeComponent();
+
+            //apply current theme
+            this.BackColor = txtRGB.BackColor = txtHEX.BackColor = LocalSetting.Theme.BackgroundColor;
+            lblPixel.ForeColor = lblRgb.ForeColor = lblHex.ForeColor = LocalSetting.Theme.TextInfoColor;
+
 
             // set the opacity and events to manage it
             Opacity = FormOpacity;
@@ -33,6 +42,8 @@ namespace ImageGlass
             {
                 ((Control)child).MouseEnter += frmColorPicker_MouseEnter;
             }
+
+            DefaultLocationOffset = new Point((int)(DefaultLocationOffset.X * DPIScaling.GetDPIScaleFactor()), (int)(DefaultLocationOffset.Y * DPIScaling.GetDPIScaleFactor()));
         }
 
         public void SetImageBox(ImageBox imgBox)
@@ -48,7 +59,105 @@ namespace ImageGlass
 
             _imgBox.MouseMove += _imgBox_MouseMove;
             _imgBox.ImageChanged += _imgBox_ImageChanged;
+            _imgBox.Click += _imgBox_Click;
         }
+
+
+        #region Borderless form moving
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        private void frmColorPicker_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+
+        #region Create shadow for borderless form
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect, // x-coordinate of upper-left corner
+            int nTopRect, // y-coordinate of upper-left corner
+            int nRightRect, // x-coordinate of lower-right corner
+            int nBottomRect, // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+
+        private bool m_aeroEnabled = false;              // variables for box shadow
+        private const int CS_DROPSHADOW = 0x00020000;
+        private const int WM_NCPAINT = 0x0085;
+        private const int WM_ACTIVATEAPP = 0x001C;
+
+        public struct MARGINS                           // struct for box shadow
+        {
+            public int leftWidth;
+            public int rightWidth;
+            public int topHeight;
+            public int bottomHeight;
+        }
+
+        private bool CheckAeroEnabled()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                int enabled = 0;
+                DwmIsCompositionEnabled(ref enabled);
+                return (enabled == 1) ? true : false;
+            }
+            return false;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCPAINT: // box shadow
+                    if (m_aeroEnabled)
+                    {
+                        var v = 2;
+                        DwmSetWindowAttribute(this.Handle, 2, ref v, 4);
+
+                        MARGINS margins = new MARGINS()
+                        {
+                            bottomHeight = 1,
+                            leftWidth = 1,
+                            rightWidth = 1,
+                            topHeight = 1
+                        };
+
+                        DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            base.WndProc(ref m);
+        }
+        #endregion
+        
 
         #region Properties to make a tool window
 
@@ -56,6 +165,7 @@ namespace ImageGlass
         {
             get { return true; }
         }
+
         protected override CreateParams CreateParams
         {
             get
@@ -63,22 +173,48 @@ namespace ImageGlass
                 CreateParams baseParams = base.CreateParams;
                 baseParams.ExStyle |= 0x8000000 // WS_EX_NOACTIVATE
                     | 0x00000080;   // WS_EX_TOOLWINDOW
+
+
+                #region Shadow for Borderless form
+                m_aeroEnabled = CheckAeroEnabled();
+
+                if (!m_aeroEnabled)
+                    baseParams.ClassStyle |= CS_DROPSHADOW;
+                #endregion
+
+
                 return baseParams;
             }
         }
 
         #endregion
 
+
         #region Events to manage the form location
+        
+        private Point parentLocation = Point.Empty;
+        private Point parentOffset = Point.Empty;
+        private bool formOwnerMoving = false;
+
+
 
         private void _AttachEventsToParent(Form frmOwner)
         {
             if (frmOwner == null)
                 return;
 
+            parentLocation = this.Owner.Location;
+
             frmOwner.Move += Owner_Move;
             frmOwner.SizeChanged += Owner_Move;
             frmOwner.VisibleChanged += Owner_Move;
+            frmOwner.Deactivate += FrmOwner_Deactivate;
+            frmOwner.LocationChanged += FrmOwner_LocationChanged;
+        }
+
+        private void FrmOwner_LocationChanged(object sender, EventArgs e)
+        {
+            formOwnerMoving = false;
         }
 
         private void _DetachEventsFromParent(Form frmOwner)
@@ -89,11 +225,32 @@ namespace ImageGlass
             frmOwner.Move -= Owner_Move;
             frmOwner.SizeChanged -= Owner_Move;
             frmOwner.VisibleChanged -= Owner_Move;
+            frmOwner.Deactivate -= FrmOwner_Deactivate;
+            frmOwner.LocationChanged -= FrmOwner_LocationChanged;
+        }
+
+        private void FrmOwner_Deactivate(object sender, EventArgs e)
+        {
+            this.TopMost = false;
+        }
+
+        private void frmColorPicker_Move(object sender, EventArgs e)
+        {
+            if (!formOwnerMoving)
+            {
+                _locationOffset = new Point(this.Left - this.Owner.Left, this.Top - this.Owner.Top);
+                parentOffset = _locationOffset;
+            }
         }
 
         private void Owner_Move(object sender, EventArgs e)
         {
+            formOwnerMoving = true;
+
+            var parentOffset = new Point(this.Owner.Left - parentLocation.X, this.Owner.Top - parentLocation.Y);
+
             _SetLocationBasedOnParent();
+            parentLocation = this.Owner.Location;
         }
 
         protected override void OnShown(EventArgs e)
@@ -104,13 +261,18 @@ namespace ImageGlass
                 _currentOwner = Owner;
                 _AttachEventsToParent(_currentOwner);
             }
+            
+
+            _locationOffset = DefaultLocationOffset;
+            parentOffset = _locationOffset;
 
             _SetLocationBasedOnParent();
-
             _ResetColor();
-
+            
+            
             base.OnShown(e);
         }
+
 
         private void _SetLocationBasedOnParent()
         {
@@ -126,11 +288,13 @@ namespace ImageGlass
 
             // set location based on the main form
             Point ownerLocation = Owner.Location;
-            ownerLocation.Offset(_locationOffset);
-            Location = ownerLocation;
+            ownerLocation.Offset(parentOffset);
+
+            this.Location = ownerLocation;
         }
 
         #endregion
+
 
         #region Events to manage ImageBox
 
@@ -164,17 +328,34 @@ namespace ImageGlass
             {
                 return;
             }
+            _imgBox.Cursor = Cursors.Cross;
 
-            Point imgPixel = _imgBox.PointToImage(e.Location);
-            if (imgPixel.X >= 0 && imgPixel.Y >= 0 && imgPixel.X < _imgBox.Image.Width
-                && imgPixel.Y < _imgBox.Image.Height)
+            _cursorPos = _imgBox.PointToImage(e.Location);
+            if (_cursorPos.X >= 0 && _cursorPos.Y >= 0 && _cursorPos.X < _imgBox.Image.Width
+                && _cursorPos.Y < _imgBox.Image.Height)
             {
-                Color color = _bmpBooster.get(imgPixel.X, imgPixel.Y);
-                _DisplayColor(color, imgPixel);
+                Color color = _bmpBooster.get(_cursorPos.X, _cursorPos.Y);
+                //_DisplayColor(color, _cursorPos);
+            }
+        }
+
+        private void _imgBox_Click(object sender, EventArgs e)
+        {
+            if (_imgBox.Image == null || _bmpBooster == null)
+            {
+                return;
+            }
+
+            if (_cursorPos.X >= 0 && _cursorPos.Y >= 0 && _cursorPos.X < _imgBox.Image.Width
+                && _cursorPos.Y < _imgBox.Image.Height)
+            {
+                Color color = _bmpBooster.get(_cursorPos.X, _cursorPos.Y);
+                _DisplayColor(color, _cursorPos);
             }
         }
 
         #endregion
+
 
         #region Opacity events
 
@@ -190,23 +371,34 @@ namespace ImageGlass
 
         #endregion
 
+
         #region Display data
 
         private void _DisplayColor(Color color, Point pixel)
         {
             panelColor.BackColor = color;
-            lblPixel.Text = string.Format("X:{0} Y:{1}", pixel.X, pixel.Y);
-            lblRgb.Text = string.Format("R:{0} G:{1} B:{2}", color.R, color.G, color.B);
-            lblHex.Text = string.Format("#{0:X2}{1:X2}{2:X2}", color.R, color.G, color.B);
+            lblPixel.Text = string.Format("Pixel: ({0}, {1})", pixel.X, pixel.Y);
+            txtRGB.Text = string.Format("{0}, {1}, {2}", color.R, color.G, color.B);
+            txtHEX.Text = string.Format("#{0:X2}{1:X2}{2:X2}", color.R, color.G, color.B);
         }
 
         private void _ResetColor()
         {
             lblPixel.Text = string.Empty;
-            lblRgb.Text = string.Empty;
-            lblHex.Text = string.Empty;
+            txtRGB.Text = string.Empty;
+            txtHEX.Text = string.Empty;
         }
 
+
+        private void ColorTextbox_Click(object sender, EventArgs e)
+        {
+            var txt = (TextBox)sender;
+            txt.SelectAll();
+        }
+        
         #endregion
+
+
+
     }
 }
