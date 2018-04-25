1a5f4e84532488e6ec7aff4dc0815362ddfce852
Phap Dieu Duong <d2phap@gmail.com>
2018-04-25 21:43:19 +0800

    load theme list to combobox

--- Source/Ultilities/igcmd/frmFirstLaunch.cs
+++ Source/Ultilities/igcmd/frmFirstLaunch.cs
@@ -11,6 +11,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ImageGlass.Library;
+using ImageGlass;
+using ImageGlass.Theme;
 
 namespace igcmd
 {
@@ -21,6 +23,14 @@ namespace igcmd
             InitializeComponent();
         }
 
+
+        private List<Theme> themeList = new List<Theme>();
+
+
+
+
+        #region Events
+
         private void frmFirstLaunch_Load(object sender, EventArgs e)
         {
             //Load language list
@@ -29,14 +39,60 @@ namespace igcmd
             //Select default layout
             cmbLayout.SelectedIndex = 0;
 
-            //Select default theme
-            cmbTheme.SelectedIndex = 0;
+            //Load theme list
+            LoadThemeList();
+            
 
             //Don't run again
             GlobalSetting.SetConfig("IsRunFirstLaunchConfigurations", "False");
         }
+        
+
+        private void btnNextStep_Click(object sender, EventArgs e)
+        {
+            if (tab1.SelectedIndex == tab1.TabCount - 1)
+            {
+                LaunchImageGlass();
+                this.Close();
+            }
+
+            tab1.SelectedIndex++;
+            lblStepNumber.Text = $"Step {tab1.SelectedIndex + 1}/{tab1.TabCount}";
 
 
+            if (tab1.SelectedIndex == tab1.TabCount - 1)
+            {
+                btnNextStep.Text = "Done!";
+            }
+        }
+
+
+        private void lnkSkip_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LaunchImageGlass();
+            this.Close();
+        }
+
+
+        private void btnSetDefaultApp_Click(object sender, EventArgs e)
+        {
+            // Update extensions to registry
+            Process p = new Process();
+            p.StartInfo.FileName = Path.Combine(GlobalSetting.StartUpDir, "igtasks.exe");
+            p.StartInfo.Arguments = $"regassociations {GlobalSetting.AllImageFormats}";
+
+            try
+            {
+                p.Start();
+            }
+            catch { }
+        }
+
+        #endregion
+
+
+
+        #region Private Functions
         /// <summary>
         /// Load language list
         /// </summary>
@@ -84,34 +140,9 @@ namespace igcmd
         }
 
 
-
-
-        
-
-        private void btnNextStep_Click(object sender, EventArgs e)
-        {
-            if (tab1.SelectedIndex == tab1.TabCount - 1)
-            {
-                LaunchImageGlass();
-                this.Close();
-            }
-
-            tab1.SelectedIndex++;
-            lblStepNumber.Text = $"Step {tab1.SelectedIndex + 1}/{tab1.TabCount}";
-
-
-            if (tab1.SelectedIndex == tab1.TabCount - 1)
-            {
-                btnNextStep.Text = "Done!";
-            }
-        }
-
-        private void lnkSkip_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            LaunchImageGlass();
-            this.Close();
-        }
-
+        /// <summary>
+        /// Launch ImageGlass app
+        /// </summary>
         private void LaunchImageGlass()
         {
             var appExe = Path.Combine(GlobalSetting.StartUpDir, "ImageGlass.exe");
@@ -121,18 +152,64 @@ namespace igcmd
             p.Start();
         }
 
-        private void btnSetDefaultApp_Click(object sender, EventArgs e)
-        {
-            // Update extensions to registry
-            Process p = new Process();
-            p.StartInfo.FileName = Path.Combine(GlobalSetting.StartUpDir, "igtasks.exe");
-            p.StartInfo.Arguments = $"regassociations {GlobalSetting.AllImageFormats}";
 
-            try
+        /// <summary>
+        /// Load theme list
+        /// </summary>
+        private void LoadThemeList()
+        {
+            //add default theme
+            var defaultTheme = new Theme(Path.Combine(GlobalSetting.StartUpDir, @"DefaultTheme\config.xml"));
+            themeList.Add(defaultTheme);
+            cmbTheme.Items.Add(defaultTheme.Name);
+            cmbTheme.SelectedIndex = 0;
+
+
+            string dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"ImageGlass\Themes\");
+
+            //get the current theme
+            var currentTheme = GlobalSetting.GetConfig("Theme", "Default");
+
+
+            if (Directory.Exists(dir))
             {
-                p.Start();
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    string configFile = Path.Combine(d, "config.xml");
+
+                    if (File.Exists(configFile))
+                    {
+                        Theme th = new Theme();
+
+                        //invalid theme
+                        if (!th.LoadTheme(configFile))
+                        {
+                            continue;
+                        }
+
+                        themeList.Add(th);
+                        cmbTheme.Items.Add(th.Name);
+
+                        if (currentTheme.ToLower().CompareTo(th.ThemeConfigFilePath.ToLower()) == 0)
+                        {
+                            cmbTheme.SelectedIndex = cmbTheme.Items.Count - 1;
+                        }
+                    }
+                }
             }
-            catch { }
+            else
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            
         }
+
+
+        #endregion
+
+
+
+
     }
 }
