8639a68eff54ba11e3d17608b54b8ba642550c25
Phap Duong <d2phap@gmail.com>
2018-06-24 15:00:29 +0800

    updated Moon version

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageGlass 5")]
+[assembly: AssemblyTitle("ImageGlass 5 Moon")]
 [assembly: AssemblyDescription("A lightweight, versatile image viewer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Duong Dieu Phap")]
-[assembly: AssemblyProduct("ImageGlass 5")]
+[assembly: AssemblyProduct("ImageGlass 5 Moon")]
 [assembly: AssemblyCopyright("Copyright © 2010-2018 by Duong Dieu Phap")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.5.27")]
-[assembly: AssemblyFileVersion("5.1.5.27")]
+[assembly: AssemblyVersion("5.1.6.24")]
+[assembly: AssemblyFileVersion("5.1.6.27")]
