aeea8920eed1acdfda89ee310b70c88d67ef424f
Phap Duong <d2phap@gmail.com>
2019-12-04 23:25:26 +0800

    set prefer x64 build

--- Source/ImageGlass/resources.bat
+++ Source/ImageGlass/resources.bat
@@ -1,8 +1,20 @@
-xcopy /Q /K /D /H /Y ..\..\..\Ultilities\igcmdWin10\bin\Release\*.* .\
+
+IF NOT EXIST ..\..\..\Ultilities\igcmdWin10\bin\Release\ (
+	xcopy /Q /K /D /H /Y ..\..\..\..\Ultilities\igcmdWin10\bin\Release\*.* .\
+) ELSE (
+	xcopy /Q /K /D /H /Y ..\..\..\Ultilities\igcmdWin10\bin\Release\*.* .\
+)
+
 
 IF NOT EXIST DefaultTheme\ (
 
 	mkdir DefaultTheme
-	xcopy /Q /K /D /H /Y ..\..\..\..\Assets\Setup\DefaultTheme\*.* DefaultTheme
+
+	IF NOT EXIST ..\..\..\..\Assets\Setup\DefaultTheme\ (
+		xcopy /Q /K /D /H /Y ..\..\..\..\..\Assets\Setup\DefaultTheme\*.* DefaultTheme
+	)
+	ELSE (
+		xcopy /Q /K /D /H /Y ..\..\..\..\Assets\Setup\DefaultTheme\*.* DefaultTheme
+	)
 )
 
