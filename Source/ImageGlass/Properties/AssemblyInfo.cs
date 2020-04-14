e03a2b1dff56ac522e6baea45c753a21bd6d917b
d2phap <d2phap@gmail.com>
2020-04-14 23:16:56 +0800

    Grey theme for CheckForUpdate form
    #580

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.5.4.13")]
-[assembly: AssemblyFileVersion("7.5.4.13")]
+[assembly: AssemblyVersion("7.5.4.14")]
+[assembly: AssemblyFileVersion("7.5.4.14")]
