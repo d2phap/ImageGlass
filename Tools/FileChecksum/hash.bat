c066145255d36df8b0cafd531e6c011bf577fc7a
d2phap <d2phap@gmail.com>
2020-04-29 21:10:00 +0800

    updated Portuguese lang

--- Tools/FileChecksum/hash.bat
+++ Tools/FileChecksum/hash.bat
@@ -1,11 +1,11 @@
-fciv -both -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_7.6.4.30_x64.msi"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_7.6.4.30_x64.msi"
 
 
-fciv -both -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_7.6.4.30_x86.msi"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_7.6.4.30_x86.msi"
 
 
-fciv -both -add "../../Setup/ImageGlass_7.6.4.30_x64.zip"
+fciv -sha1 -add "../../Setup/ImageGlass_7.6.4.30_x64.zip"
 
 
-fciv -both -add "../../Setup/ImageGlass_7.6.4.30_x86.zip"
+fciv -sha1 -add "../../Setup/ImageGlass_7.6.4.30_x86.zip"
 pause
