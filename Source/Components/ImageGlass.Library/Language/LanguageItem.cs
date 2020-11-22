c8ac01e63240abdbb8ff69e98a7c256376ceab66
Phap Dieu Duong <d2phap@gmail.com>
2020-11-22 14:22:27 +0800

    update website url

--- Source/Components/ImageGlass.Library/Language/LanguageItem.cs
+++ Source/Components/ImageGlass.Library/Language/LanguageItem.cs
@@ -1,7 +1,7 @@
 ﻿/*
 ImageGlass Project - Image viewer for Windows
 Copyright (C) 2016 DUONG DIEU PHAP
-Project homepage: http://imageglass.org
+Project homepage: https://imageglass.org
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
