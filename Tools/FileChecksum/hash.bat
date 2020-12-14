bd15388fe39beddce28b4204277a0c6347b78868
Phap Dieu Duong <d2phap@gmail.com>
2020-12-14 21:39:56 +0800

    Update hash.bat

--- Tools/FileChecksum/hash.bat
+++ Tools/FileChecksum/hash.bat
@@ -1,11 +1,11 @@
 fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.0.12.8_x64.msi"
 
 
-@REM fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.0.12.8_x86.msi"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.0.12.8_x86.msi"
 
 
 fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.0.12.8_x64.zip"
 
 
-@REM fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.0.12.8_x86.zip"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.0.12.8_x86.zip"
 pause
