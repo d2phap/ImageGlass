ef2299a89a6125b83dd7bb060799fa7cc7281c6a
Phap Dieu Duong <d2phap@gmail.com>
2021-08-06 23:01:39 +0800

    add Windows 11 Corner Api

---
+++ Source/Components/ImageGlass.Library/WinAPI/CornerApi.cs
@@ -0,0 +1,65 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2021 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Runtime.InteropServices;
+using ImageGlass.Base;
+
+namespace ImageGlass.Library.WinAPI {
+    public class CornerApi {
+        /// <summary>
+        /// The enum flag for DwmSetWindowAttribute's second parameter,
+        /// which tells the function what attribute to set.
+        /// </summary>
+        private enum DWMWINDOWATTRIBUTE {
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33
+        }
+
+        /// <summary>
+        /// The DWM_WINDOW_CORNER_PREFERENCE enum for DwmSetWindowAttribute's third parameter, 
+        /// which tells the function what value of the enum to set.
+        /// </summary>
+        private enum DWM_WINDOW_CORNER_PREFERENCE {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3,
+        }
+
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern long DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attribute, ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute, uint cbAttribute);
+
+
+        /// <summary>
+        /// Apply rounded corners for Windows 11
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <returns></returns>
+        public static void ApplyCorner(IntPtr handle) {
+            if (!Helpers.IsOS(WindowsOS.Win11)) {
+                return;
+            }
+
+
+            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
+            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+
+            DwmSetWindowAttribute(handle, attribute, ref preference, sizeof(uint));
+        }
+    }
+}
