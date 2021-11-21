fd4c0b4a74eb182b69dddea9fc8844d818c9dbee
Phap Dieu Duong <ddphap@gmail.com>
2021-11-21 19:05:44 +0800

    msi build for x86

--- Tools/FileChecksum/hash.bat
+++ Tools/FileChecksum/hash.bat
@@ -1,11 +1,11 @@
-fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.2.6.6_x64.msi"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.3.11.21_x64.msi"
 
 
-fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.2.6.6_x86.msi"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.3.11.21_x86.msi"
 
 
-fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.2.6.6_x64.zip"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.3.11.21_x64.zip"
 
 
-fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.2.6.6_x86.zip"
+fciv -sha1 -add "../../Setup/AdvancedInstaller/Bin/ImageGlass_8.3.11.21_x86.zip"
 pause
