25d734331b867a21301cc17e7d8dca4398f6453e
Phap Dieu Duong <ddphap@gmail.com>
2021-12-29 12:04:19 +0800

    Base: add QueuedWorker, PersistentCache

---
+++ v9/Components/ImageGlass.Base/QueuedWorker/Events/QueuedWorkerDoWorkEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace ImageGlass.Base.QueuedWorker;
+
+
+
+/// <summary>
+/// Represents the method that will handle the DoWork event.
+/// </summary>
+/// <param name="sender">The object that is the source of the event.</param>
+/// <param name="e">An <see cref="QueuedWorkerDoWorkEventArgs"/> that contains event data.</param>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public delegate void QueuedWorkerDoWorkEventHandler(object sender, QueuedWorkerDoWorkEventArgs e);
+
+
+/// <summary>
+/// Represents the event arguments of the DoWork event.
+/// </summary>
+public class QueuedWorkerDoWorkEventArgs : DoWorkEventArgs
+{
+    /// <summary>
+    /// Gets the priority of this item.
+    /// </summary>
+    public int Priority { get; private set; }
+
+    /// <summary>
+    /// Initializes a new instance of the QueuedWorkerDoWorkEventArgs class.
+    /// </summary>
+    /// <param name="argument">The argument of an asynchronous operation.</param>
+    /// <param name="priority">A value between 0 and 5 indicating the priority of this item.</param>
+    public QueuedWorkerDoWorkEventArgs(object argument, int priority)
+        : base(argument)
+    {
+        Priority = priority;
+    }
+}
---
+++ v9/Components/ImageGlass.Base/QueuedWorker/ProcessingModeEnum.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ImageGlass.Base.QueuedWorker;
+
+/// <summary>
+/// Represents the mode in which the work items of <see cref="QueuedBackgroundWorker"/> are processed.
+/// </summary>
+public enum ProcessingMode
+{
+    /// <summary>
+    /// Items are processed in the order they are received.
+    /// </summary>
+    FIFO,
+    /// <summary>
+    /// Items are processed in reverse order.
+    /// </summary>
+    LIFO,
+}
