ece9c2465df2e80bdf6eadaea2c698b7cfbd4c59
Phap Dieu Duong <ddphap@gmail.com>
2022-01-14 17:44:02 +0800

    thumbnail bar: flashing background when app is busy

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageGlass Moon")]
+[assembly: AssemblyTitle("ImageGlass")]
 [assembly: AssemblyDescription("A lightweight, versatile image viewer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Duong Dieu Phap")]
-[assembly: AssemblyProduct("ImageGlass Moon")]
+[assembly: AssemblyProduct("ImageGlass")]
 [assembly: AssemblyCopyright("Copyright © 2010-2022 Duong Dieu Phap")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.3.12.12")]
-[assembly: AssemblyFileVersion("8.3.12.12")]
+[assembly: AssemblyVersion("8.3.12.14")]
+[assembly: AssemblyFileVersion("8.3.12.14")]
