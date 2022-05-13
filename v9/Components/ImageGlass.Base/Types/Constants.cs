edac343fda6e77b65238e53049ef091ec49dd4c5
Phap Dieu Duong <ddphap@gmail.com>
2022-05-13 12:13:38 +0800

    add formats: xv, fits

--- v9/Components/ImageGlass.Base/Types/Constants.cs
+++ v9/Components/ImageGlass.Base/Types/Constants.cs
@@ -57,7 +57,7 @@ public static class Constants
     /// <summary>
     /// Gets built-in image formats
     /// </summary>
-    public const string IMAGE_FORMATS = "*.3fr;*.ari;*.arw;*.avif;*.b64;*.bay;*.bmp;*.cap;*.cr2;*.cr3;*.crw;*.cur;*.cut;*.dcr;*.dcs;*.dds;*.dib;*.dng;*.drf;*.eip;*.emf;*.erf;*.exif;*.exr;*.fff;*.flif;*.gif;*.gpr;*.hdr;*.heic;*.heif;*.ico;*.iiq;*.jfif;*.jp2;*.jpe;*.jpeg;*.jpg;*.jxl;*.k25;*.kdc;*.mdc;*.mef;*.mjpeg;*.mos;*.mrw;*.nef;*.nrw;*.obm;*.orf;*.pbm;*.pcx;*.pef;*.pgm;*.png;*.ppm;*.psb;*.psd;*.ptx;*.pxn;*.qoi;*.r3d;*.raf;*.raw;*.rw2;*.rwl;*.rwz;*.sr2;*.srf;*.srw;*.svg;*.tga;*.tif;*.tiff;*.viff;*.vx;*.webp;*.wmf;*.wpg;*.x3f;*.xbm;*.xpm";
+    public const string IMAGE_FORMATS = "*.3fr;*.ari;*.arw;*.avif;*.b64;*.bay;*.bmp;*.cap;*.cr2;*.cr3;*.crw;*.cur;*.cut;*.dcr;*.dcs;*.dds;*.dib;*.dng;*.drf;*.eip;*.emf;*.erf;*.exif;*.exr;*.fff;*.fits;*.flif;*.gif;*.gpr;*.hdr;*.heic;*.heif;*.ico;*.iiq;*.jfif;*.jp2;*.jpe;*.jpeg;*.jpg;*.jxl;*.k25;*.kdc;*.mdc;*.mef;*.mjpeg;*.mos;*.mrw;*.nef;*.nrw;*.obm;*.orf;*.pbm;*.pcx;*.pef;*.pgm;*.png;*.ppm;*.psb;*.psd;*.ptx;*.pxn;*.qoi;*.r3d;*.raf;*.raw;*.rw2;*.rwl;*.rwz;*.sr2;*.srf;*.srw;*.svg;*.tga;*.tif;*.tiff;*.viff;*.webp;*.wmf;*.wpg;*.x3f;*.xbm;*.xpm;*.xv";
 
     /// <summary>
     /// Number format to use for save/restore ImageGlass settings
