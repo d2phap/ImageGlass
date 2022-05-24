87ebdcf1fb7850373fb3f25840e2f00178b56059
Phap Dieu Duong <ddphap@gmail.com>
2022-05-24 22:36:48 +0800

    ImageBoxEx: slice image into small tiles and draw

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.5.5.22")]
-[assembly: AssemblyFileVersion("8.5.5.22")]
+[assembly: AssemblyVersion("8.5.5.24")]
+[assembly: AssemblyFileVersion("8.5.5.24")]
