9dd4962159a79623ec80d8e25fa29aaa8caa54eb
Phap Dieu Duong <ddphap@gmail.com>
2022-06-12 15:51:31 +0800

    add igcmd10 project, target net6.0-windows10.0.19041.0

---
+++ v9/Utilities/igcmd10/igcmd10/Program.cs
@@ -0,0 +1,74 @@
+/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2022 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using ImageGlass.Base;
+using ImageGlass.Base.WinApi;
+using ImageGlass.Settings;
+using ImageGlass.UI;
+
+namespace igcmd10;
+
+internal static class Program
+{
+    public static string[] Args = Array.Empty<string>();
+
+
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static int Main(string[] args)
+    {
+        // Issue #360: IG periodically searching for dismounted device.
+        WindowApi.SetAppErrorMode();
+
+        // To customize application configuration such as set high DPI settings
+        // or default font, see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+
+        // load application configs
+        Config.Load();
+
+        Args = args;
+
+        if (args.Length == 0)
+        {
+            return Popup.ShowDefaultIgCommandError(nameof(igcmd10), Config.Theme, Config.Language);
+        }
+
+        var topCmd = args[0].ToLower().Trim();
+
+
+        #region SHARE <string imgPath> [int style]
+        if (topCmd == IgCommands.SHARE)
+        {
+            //////////
+        }
+        #endregion
+
+
+        #region SET_LOCK_SCREEN <string imgPath>
+        if (topCmd == IgCommands.SET_LOCK_SCREEN)
+        {
+        }
+        #endregion
+
+
+        return (int)IgExitCode.Error;
+    }
+}
