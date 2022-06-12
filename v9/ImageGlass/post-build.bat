c4bfbdb58de26e26b252def10b2afc568d6fac15
Phap Dieu Duong <ddphap@gmail.com>
2022-06-12 16:50:25 +0800

    DevOps: build and copy igcmd10 project on pre-build and post-build

--- v9/ImageGlass/post-build.bat
+++ v9/ImageGlass/post-build.bat
@@ -12,6 +12,8 @@ echo:
 echo Arguments: -----------------------------------------------------------
 echo "1: %1"
 echo "2: %2"
+echo "3: %3"
+echo "4: %4"
 echo "CD: %CD%"
 echo:
 echo:
@@ -19,19 +21,32 @@ echo:
 
 set SOLUTION_DIR=%1
 set OUTPUT_DIR=%2
+set BUILD_TYPE=%3
+set PLATFORM=%4
+set IGCMD10_DIR=%SOLUTION_DIR%Utilities\igcmd10\bin\%PLATFORM%\%BUILD_TYPE%\net6.0-windows10.0.19041.0\
 set THEME_DIR=%SOLUTION_DIR%..\Setup\v9\Assets\Themes\
 
 
-echo 1. Creating variables:
+echo ######## 1. Creating variables:
 echo -----------------------------------------------------------------------
 echo SOLUTION_DIR: %SOLUTION_DIR%
 echo OUTPUT_DIR: %OUTPUT_DIR%
+echo BUILD_TYPE: %BUILD_TYPE%
+echo PLATFORM: %PLATFORM%
+echo IGCMD10_DIR: %IGCMD10_DIR%
 echo THEME_DIR: %THEME_DIR%
 echo:
 echo:
 
 
-echo 3. Copy default theme: %THEME_DIR% (if not exist)
+echo ######## 2. IGCMD10_DIR: Copy files from %IGCMD10_DIR%
+echo -----------------------------------------------------------------------
+xcopy /Q /K /D /H /Y %IGCMD10_DIR%*.* %OUTPUT_DIR%
+echo:
+echo:
+
+
+echo ######## 3. THEME_DIR: Copy default theme: %THEME_DIR% (if not exist)
 echo -----------------------------------------------------------------------
 IF NOT EXIST %OUTPUT_DIR%Themes (
 	mkdir %OUTPUT_DIR%Themes
