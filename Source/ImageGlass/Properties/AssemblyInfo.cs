18bec69163b6796ad8a6aa7bf60971f62f113082
Phap Dieu Duong <ddphap@gmail.com>
2022-06-13 20:26:08 +0800

    updated setup file

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.6.6.9")]
-[assembly: AssemblyFileVersion("8.6.6.9")]
+[assembly: AssemblyVersion("8.6.7.13")]
+[assembly: AssemblyFileVersion("8.6.7.13")]
