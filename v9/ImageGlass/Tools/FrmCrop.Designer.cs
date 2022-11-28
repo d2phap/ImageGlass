6074e04e20db722eb710bd7f28a0485ac52a2ba6
Phap Dieu Duong <ddphap@gmail.com>
2022-11-28 18:34:00 +0800

    add FrmCrop

---
+++ v9/ImageGlass/Tools/FrmCrop.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ImageGlass
+{
+    partial class FrmCrop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmCrop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 249);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "FrmCrop";
+            this.Text = "[Crop tool]";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ v9/ImageGlass/Tools/FrmCrop.cs
@@ -0,0 +1,40 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2022 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using ImageGlass.Base;
+using ImageGlass.Base.WinApi;
+using ImageGlass.UI;
+
+namespace ImageGlass;
+
+public partial class FrmCrop : ToolForm
+{
+    public FrmCrop(Form owner, IgTheme theme) : base(theme)
+    {
+        InitializeComponent();
+        Owner = owner;
+
+        var padding = DpiApi.Transform(10);
+        var top = SystemInformation.CaptionHeight + Constants.TOOLBAR_ICON_HEIGHT * 2;
+
+        // set default location offset on the parent form
+        InitLocation = new Point(padding, DpiApi.Transform(top) + padding);
+    }
+
+}
