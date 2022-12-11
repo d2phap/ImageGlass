28e1400cc43dc6208f3dd1c8333c49769f6aa398
Phap Dieu Duong <ddphap@gmail.com>
2022-12-11 14:03:17 +0800

    created FrmCropSettings inherited from DialogForm

---
+++ v9/ImageGlass/Tools/FrmCropSettings.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ImageGlass
+{
+    partial class FrmCropSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableTop = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableTop
+            // 
+            this.tableTop.ColumnCount = 3;
+            this.tableTop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableTop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableTop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 143F));
+            this.tableTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableTop.Location = new System.Drawing.Point(0, 0);
+            this.tableTop.Margin = new System.Windows.Forms.Padding(0);
+            this.tableTop.Name = "tableTop";
+            this.tableTop.RowCount = 4;
+            this.tableTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableTop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableTop.Size = new System.Drawing.Size(646, 263);
+            this.tableTop.TabIndex = 0;
+            // 
+            // FrmCropSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 458);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableTop);
+            this.Name = "FrmCropSettings";
+            this.Text = "[Crop settings]";
+            this.Controls.SetChildIndex(this.tableTop, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableTop;
+    }
+}
---
+++ v9/ImageGlass/Tools/FrmCropSettings.cs
@@ -0,0 +1,83 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2023 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using ImageGlass.Base;
+using ImageGlass.Settings;
+using ImageGlass.UI;
+
+namespace ImageGlass;
+
+public partial class FrmCropSettings : DialogForm
+{
+    public CropToolConfig Settings { get; init; }
+
+
+    public FrmCropSettings(CropToolConfig settings) : base()
+    {
+        InitializeComponent();
+
+        Settings = settings;
+    }
+
+
+    // Override / Virtual methods
+    #region Override / Virtual methods
+
+    protected override void ApplyTheme(bool darkMode, BackdropStyle? style = null)
+    {
+        SuspendLayout();
+
+        if (Config.Theme != null)
+        {
+            EnableTransparent = darkMode = Config.Theme.Settings.IsDarkMode;
+        }
+
+
+        ResumeLayout(false);
+        base.ApplyTheme(darkMode, style);
+    }
+
+
+    protected override void OnRequestUpdatingColorMode(SystemColorModeChangedEventArgs e)
+    {
+        // update theme here
+        ApplyTheme(e.IsDarkMode);
+
+        base.OnRequestUpdatingColorMode(e);
+    }
+
+
+    protected override int OnUpdateHeight(bool performUpdate = true)
+    {
+        var baseHeight = base.OnUpdateHeight(false);
+        var contentHeight = tableTop.Height + tableTop.Padding.Vertical;
+
+        if (performUpdate)
+        {
+            Height = contentHeight + baseHeight;
+        }
+
+        return contentHeight;
+    }
+
+    #endregion // Override / Virtual methods
+
+
+
+}
