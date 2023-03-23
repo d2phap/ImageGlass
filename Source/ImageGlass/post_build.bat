16d5d2ddd44fb557fd74f1193bf2e85de7299a62
Phap Dieu Duong <ddphap@gmail.com>
2023-03-24 00:35:02 +0800

    rename resources.bat to post_build.bat

--- Source/ImageGlass/post_build.bat
+++ Source/ImageGlass/post_build.bat
@@ -12,13 +12,17 @@ echo:
 echo Arguments: -----------------------------------------------------------
 echo "1: %1"
 echo "2: %2"
+echo "3: %3"
 echo "CD: %CD%"
 echo:
 echo:
 
 
 set SOLUTION_DIR=%1
+:: Debug, Release, Release_MSIX
 set BUILD_CONFIG=%2
+:: x64, x86, ...
+set PLATFORM=%3
 set OUTPUT_DIR=%CD%\
 set IGCMD_WIN10_DIR=%OUTPUT_DIR:\ImageGlass\bin\=\Ultilities\igcmdWin10\bin\%
 set THEME_DIR=%SOLUTION_DIR%..\Setup\Assets\Themes\
@@ -28,6 +32,7 @@ echo 1. Creating variables:
 echo -----------------------------------------------------------------------
 echo SOLUTION_DIR: %SOLUTION_DIR%
 echo BUILD_CONFIG: %BUILD_CONFIG%
+echo PLATFORM: %PLATFORM%
 echo OUTPUT_DIR: %OUTPUT_DIR%
 echo IGCMD_WIN10_DIR: %IGCMD_WIN10_DIR%
 echo THEME_DIR: %THEME_DIR%
@@ -52,6 +57,8 @@ echo -----------------------------------------------------------------------
 IF NOT EXIST Themes\ (
 	mkdir Themes
 	xcopy /Q /K /D /H /E /Y %THEME_DIR%*.* Themes
+) ELSE (
+  echo Skip!
 )
 echo:
 echo:
