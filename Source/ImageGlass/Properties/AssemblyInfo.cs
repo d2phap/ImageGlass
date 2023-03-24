74cc3c2901062931e9369987eb0362cbaccbf105
Phap Dieu Duong <ddphap@gmail.com>
2023-03-24 23:12:31 +0800

    bump version

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Duong Dieu Phap")]
 [assembly: AssemblyProduct("ImageGlass")]
-[assembly: AssemblyCopyright("Copyright © 2010-2022 Duong Dieu Phap")]
+[assembly: AssemblyCopyright("Copyright © 2010-2023 Duong Dieu Phap")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.8.3.28")]
-[assembly: AssemblyFileVersion("8.8.3.28")]
+[assembly: AssemblyVersion("8.8.3.29")]
+[assembly: AssemblyFileVersion("8.8.3.29")]
