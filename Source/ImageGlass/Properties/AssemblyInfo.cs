bbf10c68b927756fb7e7d0461d4d2a6a42af489a
Phap Dieu Duong <ddphap@gmail.com>
2023-03-24 23:42:06 +0800

    revert version number back to 8.8.3.28

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.8.3.29")]
-[assembly: AssemblyFileVersion("8.8.3.29")]
+[assembly: AssemblyVersion("8.8.3.28")]
+[assembly: AssemblyFileVersion("8.8.3.28")]
