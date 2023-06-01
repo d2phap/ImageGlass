c7e90b6dedb589e914baa05446fccb4606fab455
Phap Dieu Duong <ddphap@gmail.com>
2023-06-01 23:56:52 +0800

    Settings: updated General

--- v9/Components/ImageGlass.Base/Language/IgLang.cs
+++ v9/Components/ImageGlass.Base/Language/IgLang.cs
@@ -451,9 +451,9 @@ public class IgLang : Dictionary<string, string>
 
         #region Tab General
         // General > General
-        Add("FrmSettings.Tab.General._StartupDir", "Startup location");
-        Add("FrmSettings.Tab.General._ConfigDir", "Config location");
-        Add("FrmSettings.Tab.General._UserConfigFile", "User settings file");
+        Add("FrmSettings.Tab.General._StartupDir", "Startup location:");
+        Add("FrmSettings.Tab.General._ConfigDir", "Config location:");
+        Add("FrmSettings.Tab.General._UserConfigFile", "User settings file:");
 
         // General > Startup
         Add("FrmSettings.Tab.General._Startup", "Startup");
