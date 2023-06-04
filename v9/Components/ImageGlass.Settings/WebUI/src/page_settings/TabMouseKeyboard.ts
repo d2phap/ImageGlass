fa4cc8ddd48a1f694b6c72fa9a549309c05ae8d6
Phap Dieu Duong <ddphap@gmail.com>
2023-06-04 10:41:06 +0800

    Settings: save settings for mouse wheel actions

--- v9/Components/ImageGlass.Settings/WebUI/src/page_settings/TabMouseKeyboard.ts
+++ v9/Components/ImageGlass.Settings/WebUI/src/page_settings/TabMouseKeyboard.ts
@@ -24,7 +24,36 @@ export default class TabMouseKeyboard {
    * Save settings as JSON object.
    */
   static exportSettings() {
-    return getChangedSettingsFromTab('mouse_keyboard');
+    const settings = getChangedSettingsFromTab('mouse_keyboard');
+
+    // MouseWheelActions
+    const newWheelScrollValue = query<HTMLSelectElement>('#Cmb_MouseWheel_Scroll').value;
+    const newWheelCtrlAndScrollValue = query<HTMLSelectElement>('#Cmb_MouseWheel_CtrlAndScroll').value;
+    const newWheelShiftAndScrollValue = query<HTMLSelectElement>('#Cmb_MouseWheel_ShiftAndScroll').value;
+    const newWheelAltAndScrollValue = query<HTMLSelectElement>('#Cmb_MouseWheel_AltAndScroll').value;
+
+    const mouseWheelActions: Record<string, string> = {};
+    if (newWheelScrollValue !== _pageSettings.config.MouseWheelActions?.Scroll) {
+      mouseWheelActions.Scroll = newWheelScrollValue;
+    }
+    if (newWheelCtrlAndScrollValue !== _pageSettings.config.MouseWheelActions?.CtrlAndScroll) {
+      mouseWheelActions.CtrlAndScroll = newWheelCtrlAndScrollValue;
+    }
+    if (newWheelShiftAndScrollValue !== _pageSettings.config.MouseWheelActions?.ShiftAndScroll) {
+      mouseWheelActions.ShiftAndScroll = newWheelShiftAndScrollValue;
+    }
+    if (newWheelAltAndScrollValue !== _pageSettings.config.MouseWheelActions?.AltAndScroll) {
+      mouseWheelActions.AltAndScroll = newWheelAltAndScrollValue;
+    }
+
+    if (Object.keys(mouseWheelActions).length > 0) {
+      settings.MouseWheelActions = mouseWheelActions;
+    }
+    else {
+      delete settings.MouseWheelActions;
+    }
+
+    return settings;
   }
 
 
