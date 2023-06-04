75684004efae46b122b720ff93194f8c24c935af
Phap Dieu Duong <ddphap@gmail.com>
2023-06-04 21:50:39 +0800

    Settings: load theme list

--- v9/Components/ImageGlass.Settings/WebUI/src/page_settings/TabAppearance.ts
+++ v9/Components/ImageGlass.Settings/WebUI/src/page_settings/TabAppearance.ts
@@ -5,6 +5,12 @@ export default class TabAppearance {
    * Loads settings for tab Appearance.
    */
   static loadSettings() {
+    TabAppearance.loadThemeList();
+
+    if (_pageSettings.themeList.length > 0) {
+      query<HTMLInputElement>(`[name="DarkTheme"][value="${_pageSettings.config.DarkTheme}"]`).checked = true;
+      query<HTMLInputElement>(`[name="LightTheme"][value="${_pageSettings.config.LightTheme}"]`).checked = true;
+    }
   }
 
 
@@ -22,6 +28,70 @@ export default class TabAppearance {
    */
   static exportSettings() {
     return getChangedSettingsFromTab('appearance');
+  }
+
+
+  private static loadThemeList() {
+    const themeList = _pageSettings.themeList || [];
+
+    const ulEl = query<HTMLTableElement>('#List_ThemeList');
+    let ulHtml = '';
+
+    for (const th of themeList) {
+      const liHtml = `
+        <li>
+          <div class="theme-item">
+            <div class="theme-preview">
+              <img src="${th.PreviewImage}" alt="${th.Info.Name}" />
+            </div>
+            <div class="theme-info">
+              <div class="theme-heading">
+                <div class="theme-title">
+                  <span class="theme-name">${th.Info.Name}</span>
+                  <span class="theme-version">${th.Info.Version}</span>
+                  <span class="theme-mode ${th.IsDarkMode ? 'theme-dark' : 'theme-light'}"></span>
+                </div>
+                <div class="theme-actions">
+                  <label>
+                    <input type="radio" name="DarkTheme" value="${th.FolderName}" />
+                    <span>
+                      <span>🌙</span>
+                      <span data-lang="FrmSettings.Tab.Appearance._DarkTheme">[Dark]</span> 
+                    </span>
+                  </label>
+                  <label>
+                    <input type="radio" name="LightTheme" value="${th.FolderName}" />
+                    <span>
+                      <span>☀️</span>
+                      <span data-lang="FrmSettings.Tab.Appearance._LightTheme">[Light]</span>
+                    </span>
+                  </label>
+                </div>
+              </div>
+              <div class="theme-description" title="${th.Info.Description}">${th.Info.Description}</div>
+              <div class="theme-location" title="${th.FolderPath}">${th.FolderPath}</div>
+              <div class="theme-author">
+                <span class="me-4">
+                  <span data-lang="FrmSettings.Tab.Appearance._Author">[Author]</span>:
+                  ${th.Info.Author || '?'}
+                </span>
+                <span class="me-4">
+                  <span data-lang="_._Website">[Website]</span>:
+                  ${th.Info.Website || '?'}
+                </span>
+                <span>
+                  <span data-lang="_._Email">[Email]</span>:
+                  ${th.Info.Email || '?'}
+                </span>
+              </div>
+            </div>
+          </div>
+        </li>`;
+
+      ulHtml += liHtml;
+    }
+
+    ulEl.innerHTML = ulHtml;
   }
 
 
