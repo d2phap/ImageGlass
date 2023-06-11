9fe3db1f16558fa8cdc3e35a542a814db655d4cf
Phap Dieu Duong <ddphap@gmail.com>
2023-06-11 16:38:56 +0800

    Settings: fixed install lang open twice

--- v9/Components/ImageGlass.Settings/WebUI/src/page_settings/TabLanguage.ts
+++ v9/Components/ImageGlass.Settings/WebUI/src/page_settings/TabLanguage.ts
@@ -14,17 +14,25 @@ export default class TabLanguage {
    * Adds events for tab Language.
    */
   static addEvents() {
+    query('#Cmb_LanguageList').removeEventListener('change', TabLanguage.handleLanguageChanged, false);
+    query('#Btn_RefreshLanguageList').removeEventListener('click', TabLanguage.onBtn_RefreshLanguageList, false);
+    query('#Lnk_InstallLanguage').removeEventListener('click', TabLanguage.onLnk_InstallLanguage, false);
+
     query('#Cmb_LanguageList').addEventListener('change', TabLanguage.handleLanguageChanged, false);
+    query('#Btn_RefreshLanguageList').addEventListener('click', TabLanguage.onBtn_RefreshLanguageList, false);
+    query('#Lnk_InstallLanguage').addEventListener('click', TabLanguage.onLnk_InstallLanguage, false);
+  }
 
-    query('#Btn_RefreshLanguageList').addEventListener('click', async () => {
-      const newLangList = await postAsync<ILanguage[]>('Btn_RefreshLanguageList');
-      TabLanguage.loadLanguageList(newLangList);
-    }, false);
 
-    query('#Lnk_InstallLanguage').addEventListener('click', async () => {
-      const newLangList = await postAsync<ILanguage[]>('Lnk_InstallLanguage');
-      TabLanguage.loadLanguageList(newLangList);
-    }, false);
+  private static async onBtn_RefreshLanguageList() {
+    const newLangList = await postAsync<ILanguage[]>('Btn_RefreshLanguageList');
+    TabLanguage.loadLanguageList(newLangList);
+  }
+
+
+  private static async onLnk_InstallLanguage() {
+    const newLangList = await postAsync<ILanguage[]>('Lnk_InstallLanguage');
+    TabLanguage.loadLanguageList(newLangList);
   }
 
 
