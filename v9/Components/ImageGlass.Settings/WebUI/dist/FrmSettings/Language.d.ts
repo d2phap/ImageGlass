9127652bedc0b287f6112e2dfa493c018b670c88
Phap Dieu Duong <ddphap@gmail.com>
2023-07-08 11:31:42 +0800

    deleted WebUI/dist/ folder

--- v9/Components/ImageGlass.Settings/WebUI/dist/FrmSettings/Language.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Language {
-    /**
-     * Loads language.
-     */
-    static load(): void;
-}
--- v9/Components/ImageGlass.Settings/WebUI/dist/helpers/webview.d.ts
+++
@@ -1,7 +0,0 @@
-export type WebviewEventHandlerFn = (name: string, data?: any) => void;
-export declare class Webview {
-    eventHandlers: Record<string, WebviewEventHandlerFn>;
-    addEvent(name: string, handler: WebviewEventHandlerFn): void;
-    removeEvent(name: string): void;
-    startListening(): void;
-}
