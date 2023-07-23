48dd9852f511e832423b79ae0e9ec70a8ce365f7
Phap Dieu Duong <ddphap@gmail.com>
2023-07-23 16:51:22 +0800

    add ColorExtensions.ToCIELAB()

--- v9/Components/ImageGlass.Base/BHelper/Extensions/ColorExtensions.cs
+++ v9/Components/ImageGlass.Base/BHelper/Extensions/ColorExtensions.cs
@@ -200,4 +200,57 @@ public static class ColorExtensions
     }
 
 
+    /// <summary>
+    /// Converts this color <see cref="c"/> to CIELAB.
+    /// </summary>
+    public static (float L, float A, float B, float Alpha) ToCIELAB(this Color c)
+    {
+        var xyz = new double[3];
+        var rgb = new double[3] { c.R / 255d, c.G / 255d, c.B / 255d };
+        var alpha = (float)Math.Round(c.A / 255f, 2);
+
+
+        if (rgb[0] > .04045f) rgb[0] = Math.Pow((rgb[0] + 0.055) / 1.055, 2.4);
+        else rgb[0] = rgb[0] / 12.92f;
+
+        if (rgb[1] > .04045f) rgb[1] = Math.Pow((rgb[1] + 0.055) / 1.055, 2.4);
+        else rgb[1] = rgb[1] / 12.92f;
+
+        if (rgb[2] > .04045f) rgb[2] = Math.Pow((rgb[2] + 0.055) / 1.055, 2.4);
+        else rgb[2] = rgb[2] / 12.92f;
+
+
+        rgb[0] = rgb[0] * 100.0f;
+        rgb[1] = rgb[1] * 100.0f;
+        rgb[2] = rgb[2] * 100.0f;
+
+
+        xyz[0] = ((rgb[0] * 0.412453f) + (rgb[1] * 0.357580f) + (rgb[2] * 0.180423f));
+        xyz[1] = ((rgb[0] * 0.212671f) + (rgb[1] * 0.715160f) + (rgb[2] * 0.072169f));
+        xyz[2] = ((rgb[0] * 0.019334f) + (rgb[1] * 0.119193f) + (rgb[2] * 0.950227f));
+
+
+        xyz[0] = xyz[0] / 95.047f;
+        xyz[1] = xyz[1] / 100.0f;
+        xyz[2] = xyz[2] / 108.883f;
+
+
+        if (xyz[0] > .008856f) xyz[0] = (float)Math.Pow(xyz[0], (1.0 / 3.0));
+        else xyz[0] = (xyz[0] * 7.787f) + (16.0f / 116.0f);
+
+        if (xyz[1] > .008856f) xyz[1] = (float)Math.Pow(xyz[1], 1.0 / 3.0);
+        else xyz[1] = (xyz[1] * 7.787f) + (16.0f / 116.0f);
+
+        if (xyz[2] > .008856f) xyz[2] = (float)Math.Pow(xyz[2], 1.0 / 3.0);
+        else xyz[2] = (xyz[2] * 7.787f) + (16.0f / 116.0f);
+
+
+        var l = (float)Math.Round((116.0f * xyz[1]) - 16.0f, 2);
+        var a = (float)Math.Round(500.0f * (xyz[0] - xyz[1]), 2);
+        var b = (float)Math.Round(200.0f * (xyz[1] - xyz[2]), 2);
+
+
+        return (l, a, b, alpha);
+    }
+
 }
