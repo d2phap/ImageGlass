072a99fa457a5f5e3f1bebd32edbb1f7e120bb66
Phap Dieu Duong <ddphap@gmail.com>
2023-09-25 21:57:46 +0800

    update dependencies

--- Source/ImageGlass/Properties/AssemblyInfo.cs
+++ Source/ImageGlass/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.9.6.9")]
-[assembly: AssemblyFileVersion("8.9.6.9")]
+[assembly: AssemblyVersion("8.10.9.25")]
+[assembly: AssemblyFileVersion("8.10.9.25")]
